5b5cceb8ae89811cf85a6fef051d050fc1e0c5aa
nicholasamato <nxa588@miami.edu>
2024-02-24 13:43:00 -0500

    show endpoint

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 
 const PORT = parseInt(parseInt(process.env.PORT)) || 8080;
 
+const endpointString = md5(process.env.ENDPOINT);
 
 app.use(helmet(), bodyParser.json());
 
 app.listen(PORT, () =>
-  console.log(`realizeAPI listening on ${PORT}`)
+  {
+    console.log(`realizeAPI listening on ${PORT}`)
+    console.log(`Endpoint: ${endpointString}`)
+  }
 );
