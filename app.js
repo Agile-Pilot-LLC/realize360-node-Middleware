cda20c92cf6374ea9175e328838def83ada285e2
nicholasamato <nxa588@miami.edu>
2024-02-25 11:52:07 -0500

    add some input validation

--- app.js
+++ app.js
@@ -14,33 +14,52 @@ const sendGenerationString = md5(process.env.SEND_GENERATION);
 
 const TESTMODE = true;
 
-function failRequest(res, statusCode = 401, response = {authenticated: false}){
+function failRequest(res, statusCode = 401, response = { authenticated: false }) {
   res.status(statusCode).send(response);
+}
+function validateBlockadeHeaders(headers){
+  const {
+    'content-type': contentType,
+    'user-agent': userAgent,
+  } = headers;
+
+  if(contentType === 'application/json' && userAgent === 'GuzzleHttp/7'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function validateUuid(uuid){
+  if(uuid.length === 36){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 app.post(`/${sendGenerationString}`, async (req, res) => {
-  let generationUuid = req.query.g;
-
-  if(!generationUuid){
-    failRequest(res, 400, {error: "Failed."});
+  const generationUuid = req.query.g;
+  if(!validateBlockadeHeaders(req.headers) || !validateUuid(generationUuid)){
+    failRequest(res, 400, { error: "Failed." });
   }
-  else{
-    db.checkIfUuidExists(generationUuid).then(async (result) => {
-      if(result){
-        let body = req.body;
-        let status = body.status;
-        
-        if(status == "completed"){
-         await awaitdb.saveBlockadeData(uuid, body);
-        }
-        
-        console.log(`Webhook Hit by Blockade API, status "${status} for generation ID: ${generationUuid}`);
-        res.status(200).send("Received :) - Thanks!");
+  else {
+    let uuidExists = await db.checkIfUuidExists(generationUuid);
+    if (uuidExists) {
+      let body = req.body;
+      let status = body.status;
+      console.log(`Webhook Hit by Blockade API, status "${status} for generation ID: ${generationUuid}`);
+      res.status(200).send("Received Request:) - Thanks Blockade!");
+
+      if (status == "completed") {
+        await awaitdb.saveBlockadeData(uuid, body);
       }
-      else{
-        failRequest(res, 400, {error: "Failed."});
-      }
-    });
+    }
+    else {
+      failRequest(res, 400, { error: "Failed." });
+    }
   }
 });
 
@@ -48,7 +67,7 @@ app.get(`/${endpointString}`, async (req, res) => {
   // Step 1: Request made to the server
   const { appId, appSecret, nonce, userId, prompt } = req.query;
 
-  if(!appId || !appSecret || !nonce || !userId || !prompt){
+  if (!appId || !appSecret || !nonce || !userId || !prompt) {
     failRequest(res);
   }
 
@@ -57,32 +76,38 @@ app.get(`/${endpointString}`, async (req, res) => {
 
   // Step 2: Authenticate the user via Oculus API
   await authenticateUser(axios, appId, appSecret, nonce, userId, TESTMODE).then(async (result) => {
-    if(result){
+    if (result) {
       // Step 3: Get user info/access permissions from Realize Database
       await getUserInfo(axios, userId, TESTMODE).then(async (userInfo) => {
-        if(userInfo.access === "full"){
+        if (userInfo.access === "full") {
           console.log("Received user info, full access. Calling Blockade API");
           const generationUuid = uuidv4();
           await db.storeUuid(generationUuid);
           // Step 4: Get 360 Image from Blockade API
           await get360Image(axios, prompt, generationUuid).then(async (response) => {
-            if(response){
+            if (response) {
               //Step 5: Create webhook post endpoint for blockade api to call when image generation is done
               console.log('Received response from Blockade API, polling DB for image');
-              
-              res.status(200).send({authenticated: true, access: "full", response: response});
+
+              res.status(200).send({ authenticated: true, access: "full", response: response });
 
               // Step 6: Poll the database for the image after waiting an initial 30 seconds
             }
-            else{
-              failRequest(res, 500, {authenticated: true, access: "full", response: response});
-            }})}
-        else{
+            else {
+              failRequest(res, 500, { authenticated: true, access: "full", response: response });
+            }
+          })
+        }
+        else {
           failRequest(res);
-        }})}
-    else{
+        }
+      })
+    }
+    else {
       failRequest(res);
-}})});
+    }
+  })
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
