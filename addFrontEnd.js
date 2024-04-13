6068f537124482ba51676521eb2a2f6027fa199b
Nick Amato <nxa588@miami.edu>
2024-04-13 15:18:40 -0400

    add back res cause its needed

--- addFrontEnd.js
+++ addFrontEnd.js
@@ -6,13 +6,13 @@ const SERVERSTATUSCODE = 1;
 const frontEndHtmlPathRoot = '/frontend/html';
 
 function addFrontEnd(app){
-    app.get('/status', (res) => {  
+    app.get('/status', (req, res) => {  
         res.status(200).send(`${SERVERSTATUSCODE}`);
     });
-    app.get('/privacy-policy', (res) => {
+    app.get('/privacy-policy', (req, res) => {
         res.sendFile(path.join(__dirname, `${frontEndHtmlPathRoot}/privacypolicy.html`));
     });
-    app.get('/', (res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, `${frontEndHtmlPathRoot}/homepage.html`));
     });
 }
