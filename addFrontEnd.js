68d55ea1d0591806f0af42d833069cde6118f306
Nick Amato <nxa588@miami.edu>
2024-04-13 15:04:55 -0400

    fix frontend paths

--- addFrontEnd.js
+++ addFrontEnd.js
@@ -1,12 +1,18 @@
+// 0 = Down, reject users
+  // 1 = Up, accept users
+const SERVERSTATUSCODE = 1;
+
+const frontEndHtmlPathRoot = '/frontend/html';
+
 function addFrontEnd(app){
     app.get('/status', (req, res) => {  
         res.status(200).send(`${SERVERSTATUSCODE}`);
     });
     app.get('/privacy-policy', (req, res) => {
-        res.sendFile(path.join(__dirname, 'privacypolicy.html'));
+        res.sendFile(path.join(__dirname, `${frontEndHtmlPathRoot}/privacypolicy.html`));
     });
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'homepage.html'));
+        res.sendFile(path.join(__dirname, `${frontEndHtmlPathRoot}/homepage.html`));
     });
 }
 
